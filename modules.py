1454787ec7eb78f9aaeda82dea3d1534b5747ace
kimbexe <kimberlyjrw@gmail.com>
2024-12-05 17:40:15 -0500

    updated movements

--- modules.py
+++ modules.py
@@ -1,7 +1,6 @@
 from nanonav import BLE, NanoBot
 import time
 
-# Create a NanoBot object
 robot = NanoBot()
 
 #travel directions 1 square each
@@ -10,65 +9,80 @@ def n(robot):
         if not robot.ir_left() and not robot.ir_right():
             robot.m1_forward(20)
             robot.m2_forward(20)
-            time.sleep(0.1)
+            time.sleep(0.05)
             robot.stop()
             time.sleep(0)
         elif robot.ir_left() and not robot.ir_right():
-            robot.m2_forward(20)
-            robot.m1_backward(20)
+            robot.m1_forward(15)
+            robot.m2_backward(15)
             time.sleep(0.1)
             robot.stop()
             time.sleep(0)
         elif not robot.ir_left() and robot.ir_right():
-            robot.m1_forward(20)
-            robot.m2_backward(20)
+            robot.m2_forward(15)
+            robot.m1_backward(15)
             time.sleep(0.1)
             robot.stop()
             time.sleep(0)
         elif robot.ir_left() and robot.ir_right():
             robot.m1_forward(25)
             robot.m2_forward(25)
-            time.sleep(1.3)
+            time.sleep(0.9)
             robot.stop()
             time.sleep(0)
             break
 
 def s(robot):
+    while True:
+        if not robot.ir_left() and not robot.ir_right():
+            robot.m1_backward(20)
+            robot.m2_backward(20)
+            time.sleep(0.1)
+            robot.stop()
+            time.sleep(0)
+        elif robot.ir_left() and not robot.ir_right():
+            robot.m1_backward(20)
+            robot.m2_forward(20)
+            time.sleep(0.1)
+            robot.stop()
+            time.sleep(0)
+        elif not robot.ir_left() and robot.ir_right():
+            robot.m2_backward(20)
+            robot.m1_forward(20)
+            time.sleep(0.1)
+            robot.stop()
+            time.sleep(0)
+        elif robot.ir_left() and robot.ir_right():
+            robot.m1_backward(25)
+            robot.m2_backward(25)
+            time.sleep(0.6)
+            robot.stop()
+            time.sleep(0)
+            break
+
+def e(robot):
     robot.m1_backward(20)
     robot.m2_forward(20)
-    time.sleep(1.8)
+    time.sleep(0.75)
     robot.stop()
     time.sleep(0)
     n(robot)
-    robot.m1_backward(20)
-    robot.m2_forward(20)
-    time.sleep(1.8)
-    robot.stop()
-    time.sleep(0)
-
-def e(robot):   #2
     robot.m2_backward(20)
     robot.m1_forward(20)
-    time.sleep(0.9)
-    robot.stop()
-    time.sleep(0)
-    n(robot)
-    robot.m1_backward(20)
-    robot.m2_forward(20)
-    time.sleep(0.9)
+    time.sleep(0.7)
     robot.stop()
     time.sleep(0)
 
 def w(robot):
-    robot.m1_backward(20)
-    robot.m2_forward(20)
-    time.sleep(0.9)
+    robot.m2_backward(20)
+    robot.m1_forward(20)
+    time.sleep(0.75)
     robot.stop()
     time.sleep(0)
     n(robot)
-    robot.m2_backward(20)
-    robot.m1_forward(20)
-    time.sleep(0.9)
+    robot.m1_backward(20)
+    robot.m2_forward(20)
+    time.sleep(0.75)
     robot.stop()
     time.sleep(0)
 
@@ -83,10 +97,9 @@ def home(robot):
         elif movement[i] == 'w':
             e(robot)
 
+movement = []       #stores order of movements
 ble = BLE(name="LDK")
 ble.send(4)
-response = ble.read()
-movement = []
 
 while True:
     response = ble.read()
@@ -119,10 +132,16 @@ while True:
         ble.send(4)
         response = ble.read()
 
-    #waiting for response
+    #wait for a response
     elif response == 4:
         response = ble.read()
 
-    #found gold, retrace steps to starting position
+    #command doesn't execute
     elif response == 5:
+        movement.pop()      #remove failed movement
+        ble.send(4)
+        response = ble.read()
+
+    #found gold, retrace steps to starting position
+    elif response == 6:
         home(robot)
